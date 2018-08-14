44ed0a99bd29809404b10c0e292248983822d8b5
washix78 <moritzmoretz26@gmail.com>
2018-08-14 10:01:55 +0900

    save (list name, extension)

--- mogura.js
+++ mogura.js
@@ -1,12 +1,33 @@
 'use strict';
 
+var dateformat = require('dateformat');
 var fs = require('fs');
 var config = require('config');
 var log4js = require('log4js');
 var os = require('os');
 var path = require('path');
 
-log4js.configure('./config/log4js.json');
+
+var timestamp = dateformat(new Date(), 'yymmddHHMMssl');
+
+log4js.configure({
+  'appenders': {
+    'stdout': {
+      'type': 'stdout'
+    },
+    'file': {
+      'type': 'file',
+      'filename': './logs/mogura-' + timestamp
+    }
+  },
+  'categories': {
+    'default': {
+      'appenders': [ 'stdout', 'file' ],
+      'level': config.logLevel
+    }
+  }
+});
+
 var logger = log4js.getLogger('default');
 
 try {
@@ -24,7 +45,27 @@ try {
     throw new Error('Unsupported type.')
   }
 
-  var ws = fs.createWriteStream(config.list.out).on('error', (err) => {
+  var opt = { extensions: null, names: null };
+  if (6 <= process.argv.length) {
+    switch (process.argv[4]) {
+    case '-e':
+      opt.extensions = [ process.argv[5] ];
+      break;
+    case '-n':
+      opt.names = [ process.argv[5] ];
+      break;
+    case '-eg':
+      opt.extensions = config.list.extensions[process.argv[5]];
+      break;
+    case '-ng':
+      opt.names = config.list.names[process.argv[5]];
+      break;
+    default:
+      throw new Error('Unsuported option type.');
+    }
+  }
+
+  var ws = fs.createWriteStream('./logs/list-' + timestamp + '.txt').on('error', (err) => {
     throw err;
   });
 
@@ -35,7 +76,9 @@ try {
 
     var dpaths = [];
     var fpaths = [];
-    fs.readdirSync(rootDpath).forEach((testPath) => {
+    fs.readdirSync(rootDpath).map((testPath) => {
+      return path.resolve(rootDpath, testPath);
+    }).forEach((testPath) => {
       if (fs.statSync(testPath).isDirectory()) {
         dpaths.push(testPath);
       } else {
@@ -45,6 +88,21 @@ try {
 
     fileCount += fpaths.length;
 
+    if (opt.extensions !== null) {
+      fpaths = fpaths.filter((fpath) => {
+        var i = fpath.lastIndexOf('.');
+        if (i === -1) {
+          return false;
+        }
+        var extension = fpath.substr(i + 1);
+        return opt.extensions.indexOf(extension) !== -1;
+      });
+    } else if (opt.names !== null) {
+      fpaths = fpaths.filter((fpath) => {
+        var name = path.basename(fpath);
+        return opt.names.indexOf(name) !== -1;
+      });
+    }
     fpaths.forEach((fpath) => {
       ws.write(fpath);
       ws.write(os.EOL);
