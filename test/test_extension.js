4596df57e47baf23fc4023808a33a716df8fb78c
washix78 <moritzmoretz26@gmail.com>
2021-12-17 12:50:11 +0900

    modify extension test

--- test/test_extension.js
+++ test/test_extension.js
@@ -6,6 +6,48 @@ const utility = require('../utility');
 const startTime = Date.now();
 const timestamp = utility.getTimestamp(startTime);
 
+const baseDpath = path.resolve(process.cwd(), 'testwork/extension');
+
+const resourceFpaths = [
+  'file0:file0_0',
+  'file0:dir1.txt/file0_1',
+  'file0:dir2.html/file0_2',
+  'file0:dir2.html/dir1/file0_3',
+  'file0:dir2.html/dir2/file0_4',
+  'file0:file0_5',
+  'file0:dir1.txt/file0_6',
+  'file0:dir2.html/file0_7',
+  'file0:dir2.html/dir1/file0_8',
+  'file0:dir2.html/dir2/file0_9',
+  'file1:file1_00.',
+  'file1:dir1.txt/file1_01.',
+  'file1:dir2.html/file1_02.',
+  'file1:dir2.html/dir1/file1_03.',
+  'file1:dir2.html/dir2/file1_04.',
+  'file1:file1_05.',
+  'file1:dir1.txt/file1_06.',
+  'file1:dir2.html/file1_07.',
+  'file1:dir2.html/dir1/file1_08.',
+  'file1:dir2.html/dir2/file1_09.',
+  'file1:file1_10.',
+  'file3:dir1.txt/file3.xml',
+  'file3:dir2.html/file3.xml',
+  'file3:dir2.html/dir1/99_76543210987654321-00_12345678901234567-file3.xml',
+  'file3:dir2.html/dir2/file3.x.xml',
+  'file4:file4_0.TXT',
+  'file4:dir1.txt/file4_1.txt',
+  'file4:dir2.html/file4_2.TXT',
+  'file5:dir2.html/dir1/file5_0.html',
+  'file5:dir2.html/dir2/file5_1.HTML',
+  'file5:file5_2.html'
+];
+const resourceSlpaths = [
+  'file6:dir1.txt/syml6',
+  'file7:dir2.html/syml7.',
+  'file8:dir2.html/dir1/syml8.txt',
+  'file9:dir2.html/dir2/syml9.HTML'
+];
+
 const expectRecordList = [
   'ext_none.d/0_12345678901234567-file0_0:file0_0',
   'ext_none.d/1_12345678901234567-file0_1:dir1.txt/file0_1',
@@ -41,44 +83,44 @@ const expectRecordList = [
 ];
 const recordRegExp = /^(?<ext>\w+|ext_none\.d|ext_zero\.d)\/(?<no>\d+)_\d{17}\-(?<newName>.+)\:(?<oldPath>.+)$/;
 const expectBeforeTargetFpathList = [
-  `${process.cwd()}/testwork/extension/file0_0`,
-  `${process.cwd()}/testwork/extension/dir1.txt/file0_1`,
-  `${process.cwd()}/testwork/extension/dir2.html/file0_2`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir1/file0_3`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir2/file0_4`,
-  `${process.cwd()}/testwork/extension/file0_5`,
-  `${process.cwd()}/testwork/extension/dir1.txt/file0_6`,
-  `${process.cwd()}/testwork/extension/dir2.html/file0_7`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir1/file0_8`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir2/file0_9`,
-  `${process.cwd()}/testwork/extension/file1_00.`,
-  `${process.cwd()}/testwork/extension/dir1.txt/file1_01.`,
-  `${process.cwd()}/testwork/extension/dir2.html/file1_02.`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir1/file1_03.`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir2/file1_04.`,
-  `${process.cwd()}/testwork/extension/file1_05.`,
-  `${process.cwd()}/testwork/extension/dir1.txt/file1_06.`,
-  `${process.cwd()}/testwork/extension/dir2.html/file1_07.`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir1/file1_08.`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir2/file1_09.`,
-  `${process.cwd()}/testwork/extension/file1_10.`,
-  `${process.cwd()}/testwork/extension/dir1.txt/file3.xml`,
-  `${process.cwd()}/testwork/extension/dir2.html/file3.xml`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir1/99_76543210987654321-00_12345678901234567-file3.xml`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir2/file3.x.xml`,
-  `${process.cwd()}/testwork/extension/file4_0.TXT`,
-  `${process.cwd()}/testwork/extension/dir1.txt/file4_1.txt`,
-  `${process.cwd()}/testwork/extension/dir2.html/file4_2.TXT`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir1/file5_0.html`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir2/file5_1.HTML`,
-  `${process.cwd()}/testwork/extension/file5_2.html`
-].sort();
+  'file0_0',
+  'dir1.txt/file0_1',
+  'dir2.html/file0_2',
+  'dir2.html/dir1/file0_3',
+  'dir2.html/dir2/file0_4',
+  'file0_5',
+  'dir1.txt/file0_6',
+  'dir2.html/file0_7',
+  'dir2.html/dir1/file0_8',
+  'dir2.html/dir2/file0_9',
+  'file1_00.',
+  'dir1.txt/file1_01.',
+  'dir2.html/file1_02.',
+  'dir2.html/dir1/file1_03.',
+  'dir2.html/dir2/file1_04.',
+  'file1_05.',
+  'dir1.txt/file1_06.',
+  'dir2.html/file1_07.',
+  'dir2.html/dir1/file1_08.',
+  'dir2.html/dir2/file1_09.',
+  'file1_10.',
+  'dir1.txt/file3.xml',
+  'dir2.html/file3.xml',
+  'dir2.html/dir1/99_76543210987654321-00_12345678901234567-file3.xml',
+  'dir2.html/dir2/file3.x.xml',
+  'file4_0.TXT',
+  'dir1.txt/file4_1.txt',
+  'dir2.html/file4_2.TXT',
+  'dir2.html/dir1/file5_0.html',
+  'dir2.html/dir2/file5_1.HTML',
+  'file5_2.html'
+].map(testPath => path.resolve(baseDpath, testPath)).sort();
 const expectBeforeTargetSlpathList = [
-  `${process.cwd()}/testwork/extension/dir1.txt/syml6`,
-  `${process.cwd()}/testwork/extension/dir2.html/syml7.`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir1/syml8.txt`,
-  `${process.cwd()}/testwork/extension/dir2.html/dir2/syml9.HTML`
-].sort();
+  'dir1.txt/syml6',
+  'dir2.html/syml7.',
+  'dir2.html/dir1/syml8.txt',
+  'dir2.html/dir2/syml9.HTML'
+].map(testPath => path.resolve(baseDpath, testPath)).sort();
 const expectExtraPathList = [
   'extra.d',
   'extra.d/dir1.txt',
@@ -91,7 +133,6 @@ const expectExtraPathList = [
   'extra.d/dir2.html/dir2/syml9.HTML'
 ].sort();
 
-
 /*
  * test log
  * test target directory
@@ -102,7 +143,7 @@ const test_not_forced = async () => {
 
   // test log
   const info = require(utility.getLatestFpath('./logs', timestamp, 'extension_TEST_NOT_FORCED.json'));
-  if (info['Target directory'] !== path.resolve('./testwork/extension')) {
+  if (info['Target directory'] !== baseDpath) {
     throw new Error(`Target directory: ${info['Target directory']}`);
   }
   if (info['Target file count'] !== expectBeforeTargetFpathList.length) {
@@ -197,7 +238,7 @@ const test_forced = async () => {
 
   // test log
   const info = require(utility.getLatestFpath('./logs', timestamp, 'extension_TEST_FORCED.json'));
-  if (info['Target directory'] !== path.resolve('./testwork/extension')) {
+  if (info['Target directory'] !== baseDpath) {
     throw new Error(`Target directory: ${info['Target directory']}`);
   }
   if (info['Target file count'] !== expectBeforeTargetFpathList.length) {
@@ -320,8 +361,9 @@ const test_forced = async () => {
 };
 
 const main = async () => {
-  fs.emptyDirSync('./testwork/extension');
-  fs.copySync('./test/resources/extension', './testwork/extension');
+  fs.emptyDirSync(baseDpath);
+  utility.generateResourceFiles(baseDpath, resourceFpaths);
+  utility.generateResourceSymbolicLinks(baseDpath, resourceSlpaths);
 
   await test_not_forced();
   await test_forced();
