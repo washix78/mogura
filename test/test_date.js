cb8efbae64d4b2f372fd4193707b7d0c5a24c536
washix78 <moritzmoretz26@gmail.com>
2021-12-22 13:09:06 +0900

    modify date test

--- test/test_date.js
+++ test/test_date.js
@@ -344,7 +344,7 @@ const test_not_forced_dym = async () => {
     throw new Error(`Time: ${info['Time']}`);
   }
   // test target directory
-  const targetFpaths = utility.getFilePaths(targetDpath);
+  const targetFpaths = utility.getFilePaths(targetDpath).map(testPath => testPath.replaceAll(path.sep, '/'));
   if (targetFpaths.length !== expectBeforeTargetFpathList.length) {
     throw new Error(`${targetFpaths.length}`);
   }
@@ -354,7 +354,7 @@ const test_not_forced_dym = async () => {
       throw new Error(`${targetFpaths[i]}`);
     }
   }
-  const targetSlpaths = utility.getSymbolicLinkPaths(targetDpath);
+  const targetSlpaths = utility.getSymbolicLinkPaths(targetDpath).map(testPath => testPath.replaceAll(path.sep, '/'));
   if (targetSlpaths.length !== expectBeforeTargetSlpathList.length) {
     throw new Error(`${targetSlpaths.length}`);
   }
